bf2147aa0b5445e17aa9dafd21a8186f8fc3a34d
brutalsam <brutalsam90@gmail.com>
2025-02-22 17:16:49 +0500

    develop fixes again

--- src/s21_sscanf.c
+++ src/s21_sscanf.c
@@ -13,18 +13,19 @@ int s21_sscanf(const char *str, const char *format, ...) {
 
 int proccess_scanf(const char *str, const char *format, va_list *args) {
     const char *current = format, *start = format;
-    int str_count = s21_strlen(str);
+    // int str_count = s21_strlen(str);
+    char buffer[256]; int len  = s21_strlen(start); s21_strncpy(buffer, start, len);
     int str_i = 0;
     if (!*str) return -1;  // Return -1 if input string is empty
     while (*current) {
-        if (*str == ' ') {
-            while (isspace(*str)) str++;
-            if (*str == '\0'){
-                int *last_count = va_arg(*args, int *);
-                *last_count = str_count;
-                return str_i;  // Return current count if we've reached end of input
-            }
-        }
+        // if (*str == ' ') {
+        //     while (isspace(*str)) str++;
+        //     if (*str == '\0'){
+        //         int *last_count = va_arg(*args, int *);
+        //         *last_count = str_count;
+        //         return str_i;  // Return current count if we've reached end of input
+        //     }
+        // }
         if (*current == '%' && *(current + 1) != '\0') {
             char len_mod = 0;
             int width = 0;
@@ -42,7 +43,7 @@ int proccess_scanf(const char *str, const char *format, va_list *args) {
             else if (*current == 's') { s_specifier(args, &str, len_mod, width) ? 0 : str_i++; (void)*current++;}
             else if (*current == 'o') { o_specifier(args, &str, len_mod, width) ? 0 : str_i++;(void)*current++;}
             else if (*current == 'x' || *current == 'X') { x_specifier(args, &str, len_mod, width) ? 0 : str_i++;(void)*current++;}
-            else if (*current == 'n') { n_specifier(args, start); (void)*current++;}
+            else if (*current == 'n') { n_specifier(args, buffer, len); (void)*current++;}
             else if (*current == 'p') { p_specifier(args, &str) ? 0 : str_i++;(void)*current++;}
             else if (*current == '%') { current++; continue; }
             else { current++; continue; }
@@ -417,10 +418,19 @@ int g_specifier(va_list *args, const char **str, char len_mod, int width) {
     return 0;
 }
 
-int n_specifier(va_list *args, const char *start) {
+int n_specifier(va_list *args, char *buffer, int len) {
     int *count_ptr = va_arg(*args, int *);
     if (count_ptr == S21_NULL) return 1;
-    *count_ptr = (int)s21_strcspn(start, "%%");
+    for (int i = 0; i < len; i ++) {
+        if (buffer[i] == '%') {
+            buffer[i] = ' ';
+            buffer[i+1] = ' ';
+            break;
+        }
+        (!isspace(buffer[i])) ? (*count_ptr)++ : 0;
+        
+    }
+    // *count_ptr = (int)s21_strcspn(start, "%%");
     return 0;
 }
 
@@ -560,7 +570,7 @@ int main(int argc, char const *argv[]) {
     char input[] = "Hello   there! ";
     int s21_value1, s21_value2;
 
-    sscanf(input, "Hello  %nthere!     %n", &s21_value1, &s21_value2);
+    s21_sscanf(input, "Hello  %nthere!     %n", &s21_value1, &s21_value2);
 
 
     return 0;
